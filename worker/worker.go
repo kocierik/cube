93d1440c68b5079511116285452c7e20723f4a08
kocierik <kocierik@gmail.com>
2023-09-20 10:21:01 +0200

    feat: add stop task

--- worker/worker.go
+++ worker/worker.go
@@ -3,7 +3,10 @@ package worker
 import (
 	"cube/task"
 	"fmt"
+	"log"
+	"time"
 
+	"github.com/docker/docker/client"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 )
@@ -27,6 +30,29 @@ func (w *Worker) StartTask() {
 	fmt.Println("Start the task")
 }
 
-func (w *Worker) StopTask() {
-	fmt.Println("Stop the task")
+func (w *Worker) StopTask(t task.Task) task.DockerResult {
+	c := task.Config{
+		Name:  "test-container-1",
+		Image: "postgres:13",
+		Env: []string{
+			"POSTGRES_USER=cube",
+			"POSTGRES_PASSWORD=secret",
+		},
+	}
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+	d := task.Docker{
+		Client: dc,
+		Config: c,
+	}
+	result := d.Stop(t.ID.String())
+
+	if result.Error != nil {
+		log.Printf("Error stopping container %v: %v", d.ContainerId, result.Error.Error())
+	}
+	t.FinishTime = time.Now().UTC()
+	t.State = task.Completed
+	w.Db[t.ID] = t
+	log.Printf("Stopped and removed container %v for task %v", d.ContainerId, t.Name)
+	return result
+
 }
